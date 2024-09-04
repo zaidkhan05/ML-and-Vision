eadb7b57181d76ed56a34588c99aee4160e7340f
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-09-04 11:17:40 -0400

    hahahahahah i was using squares

--- machinevision/assignment2/transformation.py
+++ machinevision/assignment2/transformation.py
@@ -7,35 +7,32 @@ from PIL import Image
 # to test the algorithms. Comment on the similarity/difference between log transformation 
 # and power-law transformation.
 
-
-
 def logTransformation(image_array, c):
     transformedImage = np.zeros_like(image_array)
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
+    for i in range(image_array.shape[0]):
+        for j in range(image_array.shape[1]):
             transformedImage[i][j] = (c * np.log(1 + image_array[i][j]))
     transformedImage = Image.fromarray(transformedImage)
     return transformedImage
 
 def powerLawTransformation(image_array, c, gamma):
     transformedImage = np.zeros_like(image_array)
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
+    for i in range(image_array.shape[0]):
+        for j in range(image_array.shape[1]):
             transformedImage[i][j] = (c * (image_array[i][j])**gamma)
     transformedImage = Image.fromarray(transformedImage)
     return transformedImage
 
 
 # Load the original image
-image_path = "machinevision/assignment1/images/frog.jpg"
+image_path = "machinevision/assignment2/banker.jpg"
 image = Image.open(image_path).convert("L")  # Ensure it's grayscale
 imageArray = np.array(image)
 
 #do the transformations
 logTransform = logTransformation(imageArray, 50)
-powerLawTransform = powerLawTransformation(imageArray, 50, 0.9)
+powerLawTransform = powerLawTransformation(imageArray, 25, 0.6)
 #save the images
-logTransform.save("machinevision/assignment2/images/frogfourierspectrum_logTransformed.jpg")
-powerLawTransform.save("machinevision/assignment2/images/frogfourierspectrum_powerLawTransformed.jpg")
-    # print(2**(7-i))
-# fullComparisonOfFinalImages(reducedImages, image)
+logTransform.save("machinevision/assignment2/images/bankerfourierspectrum_logTransformed.jpg")
+powerLawTransform.save("machinevision/assignment2/images/bankerfourierspectrum_powerLawTransformed.jpg")
+
