7234a684c0dbd4bf338af2a9826158481699112c
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-09-16 17:22:10 -0400

    itk its broken

--- machinevision/assignment3/filter.py
+++ machinevision/assignment3/filter.py
@@ -2,22 +2,26 @@ import cv2
 import numpy as np
 
 
-def morphologicalFilter(input):
-    # outputImage = np.zeros(input.shape)
-    # for i in range(1, input.shape[0]-1):
-    #     for j in range(1, input.shape[1]-1):
-    #         outputImage[i,j] = np.max(input[i-1:i+2, j-1:j+2])
-    image = cv2.morphologyEx(input, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+def morphologicalFilter(image):
+    kernel = [[0,0,1,0,0], [0,1,1,1,0], [1,1,1,1,1], [0,1,1,1,0], [0,0,1,0,0]]
+    kernel = np.array(kernel, np.uint8)
+    ###############################################################
+    # code for output image b
+    # image = cv2.morphologyEx(input, cv2.MORPH_ERODE, kernel)
+    ###############################################################
+
+    ###############################################################
+    # code for output image d
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    ###############################################################
+
+
     return image
-    # return outputImage
 
 def medianFilter(input):
-    # outputImage = np.zeros(input.shape)
-    # for i in range(1, input.shape[0]-1):
-    #     for j in range(1, input.shape[1]-1):
-    #         outputImage[i,j] = np.median(input[i-1:i+2, j-1:j+2])
-    image = cv2.medianBlur(input, 5)
+    image = cv2.medianBlur(input, 7)
+    # image = cv2.Laplacian(input, cv2.CV_64F)
     return image
 def saveImages(images, whichImage):
     #save the images to one image canvas
@@ -30,7 +34,7 @@ def saveImages(images, whichImage):
     cv2.imwrite(f"machinevision/assignment3/comparisons/{whichImage}comparison.jpg", outputImage)
 
 inputPath = 'machinevision/assignment3/'
-whichImage = 'fingerPrint_BW'
+whichImage = 'morphology'
 image = cv2.imread(f'{inputPath}{whichImage}.png')
 images = []
 images.append(image)
