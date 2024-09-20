f6b9570505bc199789370e499f421a6693570e8b
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-09-20 13:30:15 -0400

    assignment3 done

--- machinevision/assignment3/cells.py
+++ machinevision/assignment3/cells.py
@@ -1,22 +1,17 @@
 import cv2
 import numpy as np
+#Code referenced from:
+#https://www.geeksforgeeks.org/circle-detection-using-opencv-python/
 
 # Read image.
-img = cv2.imread('C:/Users/agent/PycharmProjects/ML-and-Vision/machinevision/assignment3/cell.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('cell.jpg', cv2.IMREAD_COLOR)
 
 # Convert to grayscale.
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.equalizeHist(gray)
-# Blur using 3 * 3 kernel.
-# gray = cv2.morphologyEx(gray, cv2.MORPH_DILATE, np.ones((21, 21), np.uint8))
-
 (thresh, gray) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 gray = cv2.morphologyEx(gray, cv2.MORPH_DILATE, np.ones((19, 19), np.uint8))
-# gray = cv2.morphologyEx(gray, cv2.MORPH_ERODE, np.ones((9, 9), np.uint8))
-
 gray = cv2.blur(gray, (25, 25))
-# gray = cv2.morphologyEx(gray, cv2.MORPH_DILATE, np.ones((15, 15), np.uint8))
-# gray_blurred =
 cv2.imshow('blurred', gray)
 cv2.waitKey(0)
 
@@ -24,7 +19,7 @@ cv2.waitKey(0)
 detected_circles = cv2.HoughCircles(gray,
 				cv2.HOUGH_GRADIENT, 1, 80, param1 = 50,
 			param2 = 26, minRadius = 1, maxRadius = 50)
-
+#for figuring out if the thing works
 radii = []
 
 # Draw circles that are detected.
@@ -38,25 +33,21 @@ if detected_circles is not None:
 
 		# Draw the circumference of the circle.
 		cv2.circle(img, (a, b), r, (0, 255, 0), 2)
-		print(r)
+		# print(r)
 		radii.append(r)
 
 		# Draw a small circle (of radius 1) to show the center.
 		cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
-		# cv2.imshow("Detected Circle", img)
-		# cv2.waitKey(0)
 
-# radii.sort()
+
+#smallest radius
 print(f'min: {min(radii)}')
-# radii.sort(reverse=True)
+#largest radius
 print(f'max: {max(radii)}')
+#how many circles
 print(f'total: {len(radii)}')
 cv2.imshow('Detected Circles', img)
 cv2.waitKey(0)
-cv2.imwrite("C:/Users/agent/PycharmProjects/ML-and-Vision/machinevision/assignment3/cellcircled.jpg", img)
+cv2.imwrite("cellcircled.jpg", img)
 
 
-# For the cell.jpg image, write a code to count the total number of cells, calculate the size 
-# of each cell in pixels, and show the boundary of the biggest cell in an output image. In 
-# your code, you might use any techniques covered in this class. Hint: Thresholding, 
-# morphological filters, connected components, etc.
