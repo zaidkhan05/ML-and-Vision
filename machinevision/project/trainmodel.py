58f99e852352ba4ebc7fefe4d162809c645a5b70
ZaidKhan05 <zaidkhan5213@gmail.com>
2024-11-08 11:44:28 -0500

    thethings

--- machinevision/project/trainmodel.py
+++ machinevision/project/trainmodel.py
@@ -29,12 +29,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Use GPU
 #model
 model = models.resnet50(pretrained=True)  # Load pretrained ResNet50
 num_ftrs = model.fc.in_features  # Get number of input features for the final layer
+model.fc = nn.Linear(num_ftrs, 43)  # Replace final layer for classification of the 43 classes *do i use 43 or 42 cuz the thing could be 0 indexed*
+model.to(device)  # Move model to GPU if available
 
-transform = transforms.Compose([
-    transforms.Resize((32, 32)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
 
 # Load the training dataset
 trainset = datasets.GTSRB(root='./data', split='train', download=True, transform=transform)
@@ -43,3 +40,29 @@ trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 # Load the test dataset
 testset = datasets.GTSRB(root='./data', split='test', download=True, transform=transform)
 testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+# Create PyTorch Dataset
+class GTSRBDataset(Dataset):
+    def __init__(self, data, targets, transform=None):
+        self.data = data
+        self.targets = torch.LongTensor(targets)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.targets[index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x, y
+
+    def __len__(self):
+        return len(self.data)
+
+# Image transformations (resize, convert to tensor, normalize) for training and testing data
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
